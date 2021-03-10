ec5cb2dedafeed29447aca56c14c1ac31559d693
Xeolia <alexia.serrierb@gmail.com>
2021-03-10 22:51:56 +0100

    Ajout API Post saveData

---
+++ src/test/java/fr/esiea/ex4A/MatchesTest.java
@@ -0,0 +1,33 @@
+package fr.esiea.ex4A;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MatchesTest {
+
+    @Test
+    void getName() {
+
+    }
+
+    @Test
+    void setName() {
+    }
+
+    @Test
+    void getTwitter() {
+    }
+
+    @Test
+    void setTwitter() {
+    }
+
+    @Test
+    void getAdditionalProperties() {
+    }
+
+    @Test
+    void setAdditionalProperty() {
+    }
+}
---
+++ src/test/java/fr/esiea/ex4A/UserTest.java
@@ -0,0 +1,65 @@
+package fr.esiea.ex4A;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+class UserTest {
+
+    @Test
+    void getUserEmail() {
+
+    }
+
+    @Test
+    void setUserEmail() {
+    }
+
+    @Test
+    void getUserName() {
+
+    }
+
+    @Test
+    void setUserName() {
+    }
+
+    @Test
+    void getUserTweeter() {
+    }
+
+    @Test
+    void setUserTweeter() {
+    }
+
+    @Test
+    void getUserCountry() {
+    }
+
+    @Test
+    void setUserCountry() {
+    }
+
+    @Test
+    void getUserSex() {
+    }
+
+    @Test
+    void setUserSex() {
+    }
+
+    @Test
+    void getUserSexPref() {
+    }
+
+    @Test
+    void setUserSexPref() {
+    }
+
+    @Test
+    void getAdditionalProperties() {
+    }
+
+    @Test
+    void setAdditionalProperty() {
+    }
+}
